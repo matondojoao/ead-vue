cca078bdf2f832d85267e2e4c87469f5881b5492
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-20 06:03:25 +0100

    Criar Template Vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,15 @@ import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/campus',
+    component: () => import('@/layouts/DefaultTemplate.vue'),
+    children:[
+      {
+        path:'',
+        name:'campus.home',
+        component:HomeView
+      }
+    ]
   }
 ]
 
