380fc1041ceb108e8e3deacc96e327bdd3b5ce73
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-20 06:24:26 +0100

    Criar Template Vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/home/HomeView.vue'
+import MySupportView from '@/views/supports/MySupportView'
 
 const routes = [
   {
     path: '/campus',
     component: () => import('@/layouts/DefaultTemplate.vue'),
-    children:[
+    children: [
       {
-        path:'',
-        name:'campus.home',
-        component:HomeView
+        path: 'minhas-duvidas',
+        name: 'campus.my.supports',
+        component: MySupportView
+      },
+      {
+        path: '',
+        name: 'campus.home',
+        component: HomeView
       }
     ]
   }
---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    Listagem de Cursos
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
