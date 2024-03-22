97755518732efce35415ba2eca7fb84677bacc82
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-22 20:49:11 +0100

    Criar Página de Módulos e Aulas no Vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/home/HomeView.vue'
 import MySupportView from '@/views/supports/MySupportView'
+import ModulesAndLessonsView from '@/views/modules/ModulesAndLessonsView.vue'
 
 const routes = [
   {
     path: '/campus',
     component: () => import('@/layouts/DefaultTemplate.vue'),
     children: [
+      {
+        path: 'modulos',
+        name: 'campus.modules',
+        component: ModulesAndLessonsView
+      },
       {
         path: 'minhas-duvidas',
         name: 'campus.my.supports',
