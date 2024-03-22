a3c932b91a151b7377a8d057e11701f36a4f764a
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-22 21:20:39 +0100

    Criar Página de Módulos e Aulas no Vue 3

--- src/views/home/components/CardCourse.vue
+++ src/views/home/components/CardCourse.vue
@@ -6,7 +6,7 @@
             </span>
             <span class="title">{{ title }}</span>
             <span class="action">
-                <a href="trainings.html" :class="['btn', bgName]">Acessar</a>
+                <router-link :to="{ name: 'campus.modules' }" :class="['btn', bgName]">Acessar</router-link>
             </span>
         </span>
         <span class="dots">
@@ -20,6 +20,6 @@
 <script>
 export default {
     name: "CardCourse",
-    props:['title','image','bgName']
+    props: ['title', 'image', 'bgName']
 };
 </script>
