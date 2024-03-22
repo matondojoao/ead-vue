c88b07e70339660d440c8ae8d32b3c44f2d62922
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-22 21:00:49 +0100

    Criar Página de Módulos e Aulas no Vue 3

--- src/views/modules/ModulesAndLessonsView.vue
+++ src/views/modules/ModulesAndLessonsView.vue
@@ -10,109 +10,15 @@
         </div>
         <div class="content">
             <div class="container">
-                
+
                 <course-modules />
 
                 <div class="right">
                     <div class="content">
-                        
-                        <player-content />
-                        <div class="comments">
-                            <div class="header">
-                                <span class="title">Dúvidas</span>
-                                <button class="btn primary">
-                                    <i class="fas fa-plus"></i>
-                                    Enviar nova dúvida
-                                </button>
-                            </div>
 
-                            <div class="content">
-                                <div class="card">
-                                    <div class="commentContent main">
-                                        <span class="avatar">
-                                            <img src="images/avatars/user01.svg" alt="">
-                                        </span>
-                                        <span class="comment">
-                                            <div class="balloon">
-                                                <span class="fas fa-sort-down"></span>
-                                                <span class="owner">Fernando - 07/10/2021</span>
-                                                <span class="text">
-                                                    Donec ligula libero, sollicitudin vel libero vel, mollis porttitor turpis. Cras mattis turpis massa, sit amet fringilla diam auctor ac. Integer sit amet rutrum risus. In eleifend urna sapien, faucibus pharetra justo luctus quis. Vivamus eleifend fringilla massa
-                                                </span>
-                                            </div>
-                                        </span>
-                                        <button class="btn primary">Ver respostas</button>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="commentContent main">
-                                        <span class="avatar">
-                                            <img src="images/avatars/user01.svg" alt="">
-                                        </span>
-                                        <div class="comment">
-                                            <div class="balloon">
-                                                <span class="fas fa-sort-down"></span>
-                                                <span class="owner">Fernando - 07/10/2021</span>
-                                                <span class="text">
-                                                    In eleifend urna sapien, faucibus pharetra justo luctus quis. Vivamus eleifend fringilla massa
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <button class="btn primary">Ocultar respostas</button>
-                                    </div>
-                                    <div class="answersContent">
-                                        <div class="commentContent rightContent">
-                                            <div class="comment">
-                                                <div class="balloon">
-                                                    <span class="fas fa-sort-down"></span>
-                                                    <span class="owner">Carlos Ferreira - 07/10/2021</span>
-                                                    <span class="text">
-                                                        In eleifend urna sapien, faucibus pharetra justo luctus quis. Vivamus eleifend fringilla massa
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <span class="avatar">
-                                                <img src="images/avatars/user03.svg" alt="">
-                                            </span>
-                                        </div>
-                                        <div class="commentContent">
-                                            <span class="avatar">
-                                                <img src="images/avatars/user01.svg" alt="">
-                                            </span>
-                                            <div class="comment">
-                                                <div class="balloon">
-                                                    <span class="fas fa-sort-down"></span>
-                                                    <span class="owner">Fernando - 07/10/2021</span>
-                                                    <span class="text">
-                                                        In eleifend urna sapien, faucibus pharetra justo luctus quis. Vivamus eleifend fringilla massa
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <span class="answer">
-                                            <button class="btn primary">Responder</button>
-                                        </span>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="commentContent main">
-                                        <span class="avatar">
-                                            <img src="images/avatars/user02.svg" alt="">
-                                        </span>
-                                        <span class="comment">
-                                            <div class="balloon">
-                                                <span class="fas fa-sort-down"></span>
-                                                <span class="owner">João - 06/10/2021</span>
-                                                <span class="text">
-                                                    Integer scelerisque placerat molestie. Vivamus dignissim bibendum sapien, non suscipit arcu lobortis nec. Donec eros est, mollis quis nibh quis, pellentesque suscipit libero.
-                                                </span>
-                                            </div>
-                                        </span>
-                                        <button class="btn primary">Ver respostas</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <player-content />
+
+                        <supports-lessons />
                     </div>
                 </div>
             </div>
@@ -123,11 +29,13 @@
 <script>
 import CourseModules from './components/CourseModules.vue'
 import PlayerContent from './components/PlayerContent.vue'
+import SupportsLessons from './components/SupportsLessons.vue'
 export default {
     name: 'ModuleAndLesson',
-    components:{
+    components: {
         CourseModules,
-        PlayerContent
+        PlayerContent,
+        SupportsLessons
     }
 }
 </script>
