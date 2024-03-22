0192a41b744fd5a080078b4d40c610da7ddd8bf4
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-22 20:55:48 +0100

    Criar Página de Módulos e Aulas no Vue 3

--- src/views/modules/ModulesAndLessonsView.vue
+++ src/views/modules/ModulesAndLessonsView.vue
@@ -15,19 +15,8 @@
 
                 <div class="right">
                     <div class="content">
-                        <div class="training">
-                            <div class="card bg-laravel">
-                                <span class="icon">
-                                    <img src="images/icons/laravel.svg" alt="">
-                                </span>
-                                <span class="title">Aula 01 - LaraFood</span>
-                                <a href="trainings.html" class="btn laravel">
-                                    <i class="fas fa-chevron-left"></i>
-                                    Voltar
-                                </a>
-                            </div>
-                            <iframe width="100%" height="auto" src="https://www.youtube.com/embed/KuzeyRr74xM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
+                        
+                        <player-content />
                         <div class="comments">
                             <div class="header">
                                 <span class="title">Dúvidas</span>
@@ -133,10 +122,12 @@
 
 <script>
 import CourseModules from './components/CourseModules.vue'
+import PlayerContent from './components/PlayerContent.vue'
 export default {
     name: 'ModuleAndLesson',
     components:{
-        CourseModules
+        CourseModules,
+        PlayerContent
     }
 }
 </script>
