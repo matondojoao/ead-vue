96d092fd5a62397c4f34642a6f3fd65afe2d2d63
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-23 12:17:39 +0100

    Criar Página de Dúvidas no Vue 3

--- src/views/supports/MySupportView.vue
+++ src/views/supports/MySupportView.vue
@@ -1,12 +1,48 @@
 <template>
-    <div class="home">
-      Minhas Dúvidas
+  <div>
+    <div class="pageTitle">
+      <span class="title">Minhas Dúvidas</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'MySupportView',
+    <div class="content">
+      <div class="container">
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li>Todos</li>
+                <li>Aguardando Minha Resposta</li>
+                <li>Aguardando Professor</li>
+                <li>Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="content">
+            <div class="comments">
+              <supports-comp />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SupportsComp from '@/components/SupportsComp.vue';
+export default {
+  name: 'ModuleAndLesson',
+  components: {
+    SupportsComp
   }
-  </script>
-  
+}
+</script>
