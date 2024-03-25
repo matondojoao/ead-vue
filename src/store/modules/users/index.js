89b091cc8b47189fa8876e4db359015885fd90d7
matondojoao <matondojoaokitemoco@gmail.com>
2024-03-25 15:10:12 +0100

    Configurar Módulos no VUEX

---
+++ src/store/modules/users/index.js
@@ -0,0 +1,23 @@
+export default {
+    state: {
+        user: {
+            name: '',
+            email: ''
+        },
+        loggedIn: false
+    },
+    mutations: {
+        SET_USER(state, user) {
+            state.user = user
+            state.loggedIn = true
+        },
+        LOGOUT(state) {
+            state.user = {
+                name: '',
+                email: ''
+            }
+            state.loggedIn = false
+        }
+    }
+
+}
